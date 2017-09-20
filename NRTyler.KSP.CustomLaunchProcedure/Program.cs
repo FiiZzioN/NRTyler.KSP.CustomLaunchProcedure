a36bc935ebe9e1a4195ceedb187bd9ec428e9186
Nicholas Tyler <fiizzion@gmail.com>
2017-09-20 10:40:48 -0400

    Add project files.

---
+++ NRTyler.KSP.CustomLaunchProcedure/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRTyler.KSP.CustomLaunchProcedure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
